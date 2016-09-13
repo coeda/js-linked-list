b707272f15d506937a0d86e1ac679711be936cef
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-12 14:29:43 -1000

    added linked list function, returned object linkedlist

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,32 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+var linkedList ={};
+
+linkedList.getHead = function(){
+
+};
+
+linkedList.getTail = function(){
+
+};
+
+linkedList.add = function(){
+
+};
+
+linkedList.remove = function(){
+
+};
+
+linkedList.get = function(){
+
+};
+
+linkedList.insert = function(){
+
+};
+
+return linkedList;
 
 }
