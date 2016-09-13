af5bf9d7a02097d1c9f5af31a5d9abace80e64e3
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-12 17:11:48 -1000

    completed get functionality

--- linkedList.js
+++ linkedList.js
@@ -4,51 +4,62 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
-var module ={};
-var listHead = null;
-var listTail = null;
+let module ={};
+let listHead = null;
+let listTail = null;
+let nodeIndex = 0;
 
-module.getHead = function(){
-  return listHead;
-
-};
-
-module.getTail = function(){
-  return listTail;
-};
-
-module.add = function(a){
-  var addedNodeVariable = {
-    value: a,
-    next: null
+  module.getHead = function(){
+    return listHead;
   };
-  if (listHead === null){
-    listHead = a;
-  }
-    listTail = a;
 
-  return addedNodeVariable;
+  module.getTail = function(){
+    return listTail;
+  };
 
-};
-
-module.remove = function(b){
-  var listRemove = module.get(b);
-
-};
-
-module.get = function(n){
-  for(let node in listData){
-    if(node.value === n ){
-      return node;
+  module.add = function(a){
+    let addedNodeVariable = {
+      value: a,
+      next: null
+    };
+    if (listHead === null){
+      listHead = addedNodeVariable;
+    } else {
+      nodeIndex ++;
     }
-  }
+    if(listTail !== null){
+      listTail.next = addedNodeVariable;
+    }
+    listTail = addedNodeVariable;
+    return addedNodeVariable;
+  };
 
-};
+  module.remove = function(b){
 
-module.insert = function(){
 
-};
+  };
 
-return module;
+  module.get = function(n){
+    let foundNode = false;
+    let selectedNode = listHead;
+      if( n > nodeIndex){
+        selectedNode = false;
+      } else {
+        for(i = 0; i <= n; i++){
+          if(i === n){
+            foundNode = selectedNode;
+          } else if(foundNode.next !== null) {
+            selectedNode = selectedNode.next;
+          }
+        }
+      }
+    foundNode = selectedNode;
+    console.log(foundNode);
+    return foundNode;
+  };
 
+  module.insert = function(){
+  };
+
+  return module;
 }
